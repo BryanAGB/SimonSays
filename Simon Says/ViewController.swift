b67ca54b433e4c36e592fcb007b63e4979ad9a40
BryanAGB <bryan@bryanmansell.co.uk>
2018-04-24 12:21:41 +0100

    Initial Commit

---
+++ Simon Says/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simon Says
+//
+//  Created by Bryan Mansell on 23/04/2018.
+//  Copyright © 2018 Bryan Mansell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
